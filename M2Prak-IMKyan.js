c690ed14dafa105134f87b070f4b29b1979b8f7a
Nayla-11 <142031221+Nayla-11@users.noreply.github.com>
2024-02-01 10:27:21 +0700

    Add files via upload

---
+++ M2Prak-IMKyan.js
@@ -0,0 +1,8 @@
+function tampilkanAlert() {
+    var nama = document.getElementById('nama').value;
+    var kelas = document.getElementById('kelas').value;
+    var makanan = document.getElementById('makanan').value;
+
+    alert('Halo ' + nama + ' kelas ' + kelas + ', makanan terenak menurut kamu adalah ' + makanan);
+}
+
